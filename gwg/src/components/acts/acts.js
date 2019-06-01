c1a512888a093ae74219cd44cff2e6d2cd1ee699
droneal11015 <droneal11015@gmail.com>
2019-05-31 22:07:45 -0400

    making functional

--- gwg/src/components/acts/acts.js
+++ gwg/src/components/acts/acts.js
@@ -19,4 +19,85 @@ export default Acts;
      "notes": "likes beach stuff",
      "user_id": 1
 
+
+         else if(this.props.role == 'teacher'){
+      console.log("in teacher componentWillMount");
+      var localloginComponent=[];
+      localloginComponent.push(
+        <MuiThemeProvider>
+          <div>
+           <TextField
+             hintText="Enter your College Rollno"
+             floatingLabelText="Teacher Id"
+             onChange={(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange={(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      )
+      this.setState({menuValue:2,loginComponent:localloginComponent,loginRole:'teacher'})
+    }
+
+        else if(value == 2){
+      var localloginComponent=[];
+      loginRole='teacher';
+      localloginComponent.push(
+        <MuiThemeProvider>
+          <div>
+           <TextField
+             hintText="Enter your College Rollno"
+             floatingLabelText="Teacher Id"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      )
+    }
+
+    componentWillMount(){
+  // console.log("willmount prop values",this.props);
+  if(this.props.role != undefined){
+    if(this.props.role == 'name'){
+      console.log("in name componentWillMount");
+      var localloginComponent=[];
+      localloginComponent.push(
+        <MuiThemeProvider>
+          <div>
+           <TextField
+             hintText="Enter your email to login"
+             floatingLabelText="Email"
+             onChange = {(event,newValue) => this.setState({email:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      )
+      //this.setState({menuValue:1,loginComponent:localloginComponent,loginRole:'student'})
+    }
+
 */
--- gwg/src/components/header/login.js
+++ gwg/src/components/header/login.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-import App from '../acts/App';
+import App from '../acts/App'; 
